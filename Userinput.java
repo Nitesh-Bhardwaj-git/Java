9da9aed93127338ebe8b68d8a796a035e757fc7c
Nitesh Kumar <114833656+Nitesh-Bhardwaj-git@users.noreply.github.com>
2024-01-25 19:28:06 +0530

    Add files via upload

---
+++ Userinput.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Userinput {
+    
+    public static void main(String[] args) {
+       
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+
+        String userName = scanner.nextLine();
+
+        System.out.println("Hello, " + userName + "!");
+
+        scanner.close();
+    }
+}
+
+
