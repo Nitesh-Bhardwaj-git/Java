f52b9052bc4b237f2dd6f9db638344e0b34e302e
Nitesh Kumar <114833656+Nitesh-Bhardwaj-git@users.noreply.github.com>
2024-01-28 23:33:10 +0530

    Add files via upload

---
+++ WrappedLong.java
@@ -0,0 +1,15 @@
+public class WrappedLong {
+    
+        public static void main(String[] args) {
+            
+            long primitiveLong = 123456789L;
+    
+            Long wrappedLong = Long.valueOf(primitiveLong);
+    
+            System.out.println("Primitive Long: " + primitiveLong);
+            System.out.println("Wrapped Long: " + wrappedLong);
+        }
+    }
+    
+    
+
