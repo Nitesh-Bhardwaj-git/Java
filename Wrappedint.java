352258441e0f9afbc708ef8a4f6f84a5f98adda1
Nitesh Kumar <114833656+Nitesh-Bhardwaj-git@users.noreply.github.com>
2024-01-29 22:08:38 +0530

    Add files via upload

---
+++ Wrappedint.java
@@ -0,0 +1,14 @@
+public class Wrappedint {
+    
+       
+       
+     
+        public static void main(String[] var0) {
+           long var1 = 123456789L;
+           Long var3 = var1;
+           System.out.println("Primitive Long: " + var1);
+           System.out.println("Wrapped Long: " + var3);
+        }
+     }
+     
+
